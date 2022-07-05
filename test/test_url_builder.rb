bc9cb56b7053c59906cf0a4d985ab13a5cafbbb2
Sean Dickinson <contact@seandickinson.dev>
2022-07-05 08:11:00 -0400

    feat: initial scaffold

---
+++ test/test_url_builder.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class TestUrlBuilder < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::UrlBuilder::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
