8c08e8c67be9cc88acbb039e68181ae1094a001b
Sean Dickinson <contact@seandickinson.dev>
2022-07-05 08:57:06 -0400

    feat: add rspec

---
+++ spec/url_builder_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require "url_builder"
+
+describe UrlBuilder::Builder do
+  it "adds segments to a base url" do
+    builder = UrlBuilder::Builder.new(base_url: "www.example.com")
+    builder.add_segment("foo")
+    builder.add_segment("bar")
+    expect(builder.to_s).to eq("www.example.com/foo/bar")
+  end
+end
